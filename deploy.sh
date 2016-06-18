4892b3c76347499fb406f85d54197d09c3c4053e
Philipp Holzschneider <philipp@covicon.de>
2016-06-19 01:30:12 +0200

    forgot something

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,10 @@ mvn install:install-file -DpomFile=pom.xml -Dfile=$(ls target/*.jar) -DlocalRepo
 
 cd $HOME/deploy
 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+
 git add .
 git commit -m "travis"
 git push -f origin deploy
